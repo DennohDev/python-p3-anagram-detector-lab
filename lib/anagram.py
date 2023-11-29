8732e3eeb28cb2f45814fde104b6b890262252f6
DennohDev <=>
2023-11-29 22:52:57 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, possible_anagrams):
+        matches = []
+        for possible_anagram in possible_anagrams:
+            if sorted(self.word.lower()) == sorted(possible_anagram.lower()) and self.word.lower() != possible_anagram.lower():
+                matches.append(possible_anagram)
+        return matches
+listen = Anagram("listen")
+listen.match(['hello', 'world'])
